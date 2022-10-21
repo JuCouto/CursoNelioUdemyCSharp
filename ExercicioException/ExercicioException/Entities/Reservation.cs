bb78bf3d12dbef16df488a9d3c8f1b8d8e008b1b
Ju Couto <jnalmeida@ymail.com>
2022-10-21 17:15:04 -0300

    Exercício com Exception

---
+++ ExercicioException/ExercicioException/Entities/Reservation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioException.Entities.Exceptions;
+
+namespace ExercicioException.Entities
+{
+    internal class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public Reservation()
+        {
+
+        }
+
+        public Reservation(int roomNumber, DateTime checkin, DateTime checkout)
+        {
+
+            if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+            RoomNumber = roomNumber;
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+        public int Duration()
+        {
+           TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkin,DateTime checkout)
+        {
+            DateTime now = DateTime.Now;
+            if (checkin < now || checkout < now)
+            {
+                throw new DomainException("Reservation dates for update must be future dates");
+            }
+            else if (checkout <= checkin)
+            {
+                throw new DomainException("Check-out date must be after check-in date");
+            }
+
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+        public override string ToString()
+        {
+            return "Room: " + RoomNumber +"Check-in: " + CheckIn.ToString("dd/MM/yyyy") + "Check-out:" + CheckOut.ToString("dd/MM/yyyy") + ", "+ Duration() + " nights";
+    }
+}
---
+++ ExercicioException/ExercicioException/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using ExercicioException.Entities;
+using ExercicioException.Entities.Exceptions;
+
+try
+{
+
+
+    Console.Write("Room number: ");
+    int room = int.Parse(Console.ReadLine());
+
+    Console.Write("Check-in date (dd/MM/yyyy):");
+    DateTime checkin = DateTime.Parse(Console.ReadLine());
+
+    Console.Write("Check-out date (dd/MM/yyyy):");
+    DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+    Reservation reservation = new Reservation(room, checkin, checkout);
+
+    Console.Write("Reservation:");
+    Console.WriteLine("Reservation: " + reservation);
+
+    Console.WriteLine();
+    Console.WriteLine("Enter data to update the reservation:");
+    Console.Write("Check-in date (dd/MM/yyyy): ");
+    checkin = DateTime.Parse(Console.ReadLine());
+    Console.Write("Check-out date (dd/MM/yyyy): ");
+    checkout = DateTime.Parse(Console.ReadLine());
+
+    reservation.UpdateDates(checkin, checkout);
+    Console.WriteLine("Reservation: " + reservation);
+
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Error in format: " + e.Message);
+}
+catch (DomainException e)
+{
+    Console.WriteLine("Error in reservation: " + e.Message);
+}
+catch (Exception e)
+{
+    Console.WriteLine("Unexpected error: " + e.Message);
+}
