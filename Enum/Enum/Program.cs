782f6544c2837e67c772a6d6ca4609f617b342fd
Ju Couto <jnalmeida@ymail.com>
2022-10-21 17:11:48 -0300

    Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,21 @@
+﻿using Enum.Entities.Enum;
+using Enum.Entities;
+
+
+
+Order order = new Order
+{
+    Id = 1080,
+    Moment = DateTime.Now,
+    Status = OrderStatus.PendingPayment
+};
+
+Console.WriteLine(order);
+
+string txt = OrderStatus.PendingPayment.ToString();//coversão de enum para string
+
+OrderStatus os;
+Enum.Entities.Enum.OrderStatus.TryParse("Delivered", out os);
+
+Console.WriteLine(os);
+Console.WriteLine(txt);
