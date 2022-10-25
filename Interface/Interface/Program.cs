71adb7211f7768d5fb59ff885b709e9a0c04e946
Ju Couto <jnalmeida@ymail.com>
2022-10-25 15:13:32 -0300

    Interface

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,26 @@
+﻿using Interface.Entities;
+using Interface.Services;
+using System.Globalization;
+
+Console.WriteLine("Enter rental data");
+Console.Write("Car model: ");
+string model = Console.ReadLine();
+Console.Write("Pickup (dd/MM/yyyy HH:mm): ");
+DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+Console.Write("Return (dd/MM/yyyy HH:mm): ");
+DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+Console.Write("Enter price per hour: ");
+double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter price per day: ");
+double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+// Injeção de dependencia, qual consigo instanciar um objeto já informando outro objeto do qual ele depende
+RentalService rentalService = new RentalService(hour, day, new BrazilTaxService());
+
+rentalService.ProcessInvoice(carRental);
+
+Console.WriteLine("INVOICE:");
+Console.WriteLine(carRental.Invoice);
