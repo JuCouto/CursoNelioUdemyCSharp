51978c538da50877ebae0d588ece988e7f7957dd
Ju Couto <jnalmeida@ymail.com>
2022-10-25 18:18:10 -0300

    Get Hash Code e Equals

---
+++ GetHashCodeEquals/GetHashCodeEquals/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using GetHashCodeEquals.Entities;
+
+Client c1 = new Client { Name = "Maria", Email = "maria@gmail.com" };
+Client c2 = new Client { Name = "Maria", Email = "maria@gmail.com" };
+
+Console.WriteLine(c1.Equals(c2));
+Console.WriteLine(c1 == c2);
+Console.WriteLine(c1.GetHashCode());
+Console.WriteLine(c2.GetHashCode());
