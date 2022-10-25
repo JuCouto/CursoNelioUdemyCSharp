bd685d156d39b0aae91ecf7113c203c8d0637872
Ju Couto <jnalmeida@ymail.com>
2022-10-25 11:36:20 -0300

    Stream Writer

---
+++ StreamWriter/StreamWriter/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+string sourcePath = @"c:\Nelio\file1.txt";
+string targetPath = @"c:\Nelio\file2.txt";
+
+try
+{
+    string[] lines = File.ReadAllLines(sourcePath);
+
+    using (StreamWriter sw = File.AppendText(targetPath))
+    {
+        foreach (string line in lines)
+        {
+            sw.WriteLine(line.ToUpper());
+        }
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("An error accurred");
+    Console.WriteLine(e.Message);
+}
