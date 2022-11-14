fc91adb378cc66e064bb181d3cd6fac1463ef358
Ju Couto <jnalmeida@ymail.com>
2022-11-14 18:15:27 -0300

    Analisar o n° de logs de um arquivo

---
+++ ExercicioConjuntos/ExercicioConjuntos/Entities/LogRecord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioConjuntos.Entities
+{
+    internal class LogRecord
+    {
+        public string UserName { get; set; }
+        public DateTime Instant { get; set; }
+
+        // O critério de igualdade está somente no username, posso colocar quantos quiser.
+        public override int GetHashCode()
+        {
+            return UserName.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is LogRecord))
+            {
+                return false;
+            }
+
+            // Downcasting
+            LogRecord other = obj as LogRecord;
+            return UserName.Equals(other.UserName);
+        }
+    }
+}
---
+++ ExercicioConjuntos/ExercicioConjuntos/Program.cs
@@ -0,0 +1,29 @@
+﻿using ExercicioConjuntos.Entities;
+using System.IO;
+using System;
+using System.Collections.Generic;
+
+HashSet<LogRecord> set = new HashSet<LogRecord>();
+
+Console.Write("Enter file full path: ");
+string path = Console.ReadLine();
+try
+{
+    using (StreamReader sr = File.OpenText(path))
+    {
+        while (!sr.EndOfStream)
+        {
+            string[] line = sr.ReadLine().Split(' ');
+            string name = line[0];
+            DateTime instant = DateTime.Parse(line[1]);
+            // Usando a sintaxe com { pq não fiz construtor na classe LogRecord.
+            set.Add(new LogRecord { UserName= name, Instant = instant});
+            
+        }
+        Console.WriteLine("Total users: " + set.Count);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
