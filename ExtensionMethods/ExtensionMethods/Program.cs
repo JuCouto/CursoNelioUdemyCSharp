f4c20f3a366ba92d66ccacf63b89865f3151938b
Ju Couto <jnalmeida@ymail.com>
2022-11-14 18:46:13 -0300

    Calcular Total dias e horas - static class

---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using ExtensionMethods.Extension;
+
+DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+Console.WriteLine(dt.ElapsedTime());
+
+// Exemplo 2 gerar recorte em uma string
+String s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
